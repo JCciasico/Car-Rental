25f05b2a7925d01cc96183200c1806a9203468f4
JCCiasico <carlociasico01@gmail.com>
2022-07-28 09:58:31 +0800

    feat: car rental website

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="intro-section">
+      <NavBar/>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    
+}
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.intro-section{
+  height: 100vh;
+  background-color: #5a5a5a;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+    <section>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <img src="../assets/car-jcc.png" alt="logo image"
+                        style="width: 10em; height: 5em; justify-content: start;">
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span><i class="fas fa-bars" style="color:#fff; font-size:28px;"></i></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse border" id="navbarNavAltMarkup">
+                        <ul class="container navbar-nav justify-content-center border">
+                            <li class="nav-item">
+                                <a href="#home" class="nav-link text" aria-current="page">HOME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#about" class="nav-link text" aria-current="page">ABOUT ME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#skills" class="nav-link text" aria-current="page">SKILLS</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#portfolio" class="nav-link text" aria-current="page">PORTFOLIO</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#services" class="nav-link text" aria-current="page">SERVICES</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#contact" class="nav-link text" aria-current="page">CONTACT</a>
+                            </li>
+                        </ul>
+                        <button style="width: 8em; height: 4em;">List a Car</button>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
