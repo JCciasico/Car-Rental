b3702ee17784b97bc22a073a299b31bdb27d0d73
JCCiasico <carlociasico01@gmail.com>
2022-08-01 09:53:46 +0800

    chore: added car image

--- src/components/RentSection.vue
+++ src/components/RentSection.vue
@@ -6,43 +6,159 @@
                 <h2>Choose the Best Rent Car</h2>
             </div>
             <div class="row rent-card">
-                <div class="col">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
                     <div class="card">
-                        <img src="../assets/2503527.jpg" class="card-img-top" alt="...">
+                        <img src="../assets/aston.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                to additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">Aston Martin Luxury Cars</h5>
+                            <div class="row box">
+                                <div class="col-lg-6 col-md-12">
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                </div>
+                                <div class="col-lg-6 col-md-12">
+                                    <h6>$40.00 <span id="per-day">/ day</span></h6>
+                                </div>
+                            </div>
+                            <div class="row box">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-user icon-1"></i>
+                                     <span>4 person</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-car icon-1"></i>
+                                     <span>Auto Transmission</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-gas-pump icon-1"></i>
+                                     <span>Petrol</span>
+                                </div>
+                            </div>
+                            <button>Rent Now</button>
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
                     <div class="card">
-                        <img src="../assets/2503527.jpg" class="card-img-top" alt="...">
+                        <img src="../assets/BMW-i8-Concept.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                to additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">BMW Marvelou Luxury Car</h5>
+                            <div class="row box">
+                                <div class="col-lg-6 col-md-12">
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                </div>
+                                <div class="col-lg-6 col-md-12">
+                                    <h6>$80.00 <span id="per-day">/ day</span></h6>
+                                </div>
+                            </div>
+                            <div class="row box">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-user icon-1"></i>
+                                     <span>2 person</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-car icon-1"></i>
+                                     <span>Auto Transmission</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-gas-pump icon-1"></i>
+                                     <span>Petrol</span>
+                                </div>
+                            </div>
+                            <button>Rent Now</button>
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
                     <div class="card">
                         <img src="../assets/2503527.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                to additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">Aston Martin Luxury Cars</h5>
+                            <div class="row box">
+                                <div class="col-lg-6 col-md-12">
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                </div>
+                                <div class="col-lg-6 col-md-12">
+                                    <h6>$40.00 <span id="per-day">/ day</span></h6>
+                                </div>
+                            </div>
+                            <div class="row box">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-user icon-1"></i>
+                                     <span>4 person</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-car icon-1"></i>
+                                     <span>Auto Transmission</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-gas-pump icon-1"></i>
+                                     <span>Petrol</span>
+                                </div>
+                            </div>
+                            <button>Rent Now</button>
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
                     <div class="card">
                         <img src="../assets/2503527.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                to additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">Aston Martin Luxury Cars</h5>
+                            <div class="row box">
+                                <div class="col-lg-6 col-md-12">
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                    <i class="fa-solid fa-star icon"></i>
+                                </div>
+                                <div class="col-lg-6 col-md-12">
+                                    <h6>$40.00 <span id="per-day">/ day</span></h6>
+                                </div>
+                            </div>
+                            <div class="row box">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-user icon-1"></i>
+                                     <span>4 person</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-car icon-1"></i>
+                                     <span>Auto Transmission</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col box-item">
+                                     <i class="fa-solid fa-gas-pump icon-1"></i>
+                                     <span>Petrol</span>
+                                </div>
+                            </div>
+                            <button>Rent Now</button>
                         </div>
                     </div>
                 </div>
@@ -61,10 +177,10 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;1,500&display=swap');
 
 .rent-body{
-    padding: 4rem 0;
+    padding: 3rem 0;
 }
 .rent-card{
-    padding: 3rem 0;
+    padding: 2rem 0;
 }
 h4{
     font-family: 'Poppins','sans-serif';
@@ -72,5 +188,45 @@ h4{
     font-weight: 600;
     color: #ffbe00;
 }
+h2{
+    font-family: 'Poppins','sans-serif';
+    padding: 1rem 0;
+    font-weight: 600;
+}
+h6{
+    font-family: 'Poppins','sans-serif';
+    font-weight: 600;
+    font-size: 1.2rem;
+}
+#per-day{
+    font-weight: lighter;
+    font-size: 1rem;
+}
+.box{
+    margin-top: 20px;
+}
+.box-item{
+    display: flex;
+    align-items: center;
+    column-gap: 10px;
+}
+button{
+    background-color: #ffbe00;
+    padding: 0.5rem;
+    width: 125px;
+    font-size: 0.8rem;
+    border: none;
+    margin-top: 30px;
+}
+.icon{
+    color: #ffbe00;
+}
+.icon-1{
+    color: #ffbe00;
+}
+.img{
+    height: 200px;
+}
+
 
 </style>
